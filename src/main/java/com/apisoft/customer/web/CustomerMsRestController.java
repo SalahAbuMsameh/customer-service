9b7d8328b2cc4fafc9c2fc07d01daba199ca1c54
s_msameh@yahoo.com <sammjuba>
2021-01-30 11:18:26 +0300

    modify API's root context tpo /customers only

--- src/main/java/com/apisoft/customer/web/CustomerMsRestController.java
+++ src/main/java/com/apisoft/customer/web/CustomerMsRestController.java
@@ -33,7 +33,7 @@ import java.util.Optional;
  * @author Salah Abu Msameh
  */
 @RestController
-@RequestMapping(path = "/api/customers")
+@RequestMapping(path = "/customers")
 @Timed
 public class CustomerMsRestController {
 
